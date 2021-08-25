8b4435881c10bf775382f0491110a05eac8bf5a3
Matt Dow <matt.dow@continuum-space.com>
2021-08-25 17:11:19 -0500

    fix?

--- src/model-mixin.vue
+++ src/model-mixin.vue
@@ -338,7 +338,7 @@ export default {
           // const index = intersects.indexOf(intersection)
           // console.log('not desirable object')
           // intersects.splice(index, 1)
-        } else if (intersection.object.children[0].type === "TransformControlsGizmo") {
+        } else if (intersection.children[0].type === "TransformControlsGizmo") {
           console.log('TH!!!!!!!!!!!!!!!!!!!')
         } else {
           newIntersects.push(intersection)
